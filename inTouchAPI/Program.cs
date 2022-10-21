71e1f06cd41a9d1deb719b6b3f622d2a812ff117
luk4sz98 <lukaszwluka@interia.pl>
2022-10-21 15:19:46 +0200

    added json string enum converter

--- inTouchAPI/Program.cs
+++ inTouchAPI/Program.cs
@@ -15,12 +15,14 @@ global using Response = inTouchAPI.Dtos.Response;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
